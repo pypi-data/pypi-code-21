3913d68e7b35c035f690871c1bd7c1eff836f0fc
Tom Forbes <tom@tomforb.es>
2017-11-10 02:21:52 +0000

    {"name":"pipinit","version":"1.0.0","file":"pipinit-1.0.0.macosx-10.12-x86_64.tar.gz","path":"pipinit/pipinit-1.0.0.macosx-10.12-x86_64.tar.gz"}

---
+++ pipinit/pipinit-1.0.0.macosx-10.12-x86_64.tar.gz/Users/yuan/p/pipinit/.venv/lib/python3.6/site-packages/__init__.py
@@ -0,0 +1,48 @@
+from os import system, path, getcwd
+
+import click
+
+
+def inputWithDefault(prompt, default=''):
+    inputGot = input(prompt)
+    if inputGot == '':
+        inputGot = default
+
+    return inputGot
+
+
+@click.command()
+def cli():
+    dirName = getcwd().split('/')[-1]
+    name = inputWithDefault('Name of the project({}): '.format(dirName),
+                            dirName)
+
+    version = inputWithDefault('Current version(1.0.0): ', '1.0.0')
+
+    description = inputWithDefault('Short description: ')
+
+    url = inputWithDefault('URL: ')
+
+    author = inputWithDefault('Author: ')
+
+    author_email = inputWithDefault('Author email: ')
+
+    license = inputWithDefault('License(MIT): ', 'MIT')
+
+    python_requires = inputWithDefault('Python requires(>=3): ', '>=3')
+
+    text = '''from setuptools import setup, find_packages
+setup(
+    name='{}',
+    version='{}',
+    description='{}',
+    url='{}',
+    author='{}',
+    author_email='{}',
+    license='{}',
+    packages=find_packages(),
+    python_requires='{}'
+    )'''.format(name, version, description, url, author, author_email, license,
+                python_requires)
+    with open('setup.py', 'w') as f:
+        f.write(text)
